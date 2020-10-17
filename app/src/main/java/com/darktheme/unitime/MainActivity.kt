df004461db9693adf9454074c08d48cccd18eece
egjlmn1 <egjlmn123@gmail.com>
2020-10-17 14:38:07 +0300

    Initial commit

---
+++ app/src/main/java/com/darktheme/unitime/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.darktheme.unitime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
